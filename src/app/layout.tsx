d2836daa92a747638547eca7c12da46965b9806f
Edward Passagi <edward.passagi@gmail.com>
2025-09-23 12:09:02 -0500

    remove mentions of infra dev

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Edward Passagi',
   description: 'Edward Passagi · Software Engineer, Builder, Problem-Solver',
-  keywords: 'Edward Passagi, Software Engineer, Backend Developer, Infrastructure Engineer, Epic Systems, UIUC, Computer Science, Illinois, Urbana-Champaign',
+  keywords: 'Edward Passagi, Software Engineer, Backend Developer, Epic Systems, UIUC, Computer Science, Illinois, Urbana-Champaign',
   authors: [{ name: 'Edward Passagi' }],
   creator: 'Edward Passagi',
   publisher: 'Edward Passagi',
@@ -17,14 +17,14 @@ export const metadata: Metadata = {
     type: 'website',
     locale: 'en_US',
     url: 'https://edwardpassagi.github.io',
-    title: 'Edward Passagi - Software Engineer & Infrastructure Developer',
-    description: 'Edward Passagi is a Backend & Infrastructure Engineer with BS & MCS in Computer Science from UIUC. Ex-Epic Systems Software Developer.',
+    title: 'Edward Passagi - Software Engineer',
+    description: 'Edward Passagi is a Backend Engineer with BS & MCS in Computer Science from UIUC. Ex-Epic Systems Software Developer.',
     siteName: 'Edward Passagi Portfolio',
   },
   twitter: {
     card: 'summary',
-    title: 'Edward Passagi - Software Engineer & Infrastructure Developer',
-    description: 'Backend & Infrastructure Engineer | Ex-Epic Systems | BS & MCS CS @ UIUC',
+    title: 'Edward Passagi - Software Engineer',
+    description: 'Backend Engineer | Ex-Epic Systems | BS & MCS CS @ UIUC',
     creator: '@edwardpassagi',
   },
   verification: {
